bd0a6f7cfbba20e2e1b4a32d3b8f241d371dbe06
Daniel Abrante <danielabrante@protonmail.com>
2024-05-13 14:40:24 -0300

    chore: adjust to focus from second TextField and add paddings to list and slider

--- lib/pages/racha_list.dart
+++ lib/pages/racha_list.dart
@@ -9,8 +9,14 @@ class RachaListPage extends StatefulWidget {
 }
 
 class _RachaListPageState extends State<RachaListPage> {
-  List<String> players = [""];
+  List<String> players = [];
   int playersPerTeam = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    players.add("");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,46 +32,51 @@ class _RachaListPageState extends State<RachaListPage> {
                 shrinkWrap: true,
                 itemCount: players.length,
                 itemBuilder: (context, index) {
-                  return TextField(
-                    autofocus: true,
-                    decoration: const InputDecoration(hintText: "Jogador(a)"),
-                    keyboardType: TextInputType.name,
-                    onChanged: (value) {
-                      setState(() {
-                        players[index] = value;
-                      });
-                    },
-                    onSubmitted: (value) {
-                      String lastPlayerEntry = players[players.length - 1];
+                  return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 24),
+                      child: TextField(
+                        autofocus: players.length > 1,
+                        decoration:
+                            const InputDecoration(hintText: "Jogador(a)"),
+                        keyboardType: TextInputType.name,
+                        onChanged: (value) {
+                          setState(() {
+                            players[index] = value;
+                          });
+                        },
+                        onSubmitted: (value) {
+                          String lastPlayerEntry = players[players.length - 1];
 
-                      if (lastPlayerEntry.isNotEmpty) {
-                        setState(() {
-                          players.add("");
-                        });
-                      }
-                    },
-                  );
+                          if (lastPlayerEntry.isNotEmpty) {
+                            setState(() {
+                              players.add("");
+                            });
+                          }
+                        },
+                      ));
                 },
               ),
               if (players.length > 2)
-                Column(
-                  children: [
-                    Text(
-                        "Número de jogadores por time: ${playersPerTeam.floor()}"),
-                    Slider(
-                      value: playersPerTeam.toDouble(),
-                      min: 1.toDouble(),
-                      max: players.length.floorToDouble(),
-                      label: playersPerTeam.round().toString(),
-                      onChanged: (double value) {
-                        setState(() {
-                          playersPerTeam = value.ceil();
-                        });
-                      },
-                    ),
-                  ],
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 48),
+                  child: Column(
+                    children: [
+                      Slider(
+                        value: playersPerTeam.toDouble(),
+                        min: 1.toDouble(),
+                        max: players.length.floorToDouble(),
+                        label: playersPerTeam.round().toString(),
+                        onChanged: (double value) {
+                          setState(() {
+                            playersPerTeam = value.ceil();
+                          });
+                        },
+                      ),
+                      Text("Jogadores por time: ${playersPerTeam.floor()}"),
+                    ],
+                  ),
                 ),
-              TextButton(
+              ElevatedButton(
                   onPressed: () {
                     players.removeWhere((element) => element.isEmpty);
 
