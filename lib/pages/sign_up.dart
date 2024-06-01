4e27dfcaf51c73740da7963bbcdadd71c40ef74a
Daniel Abrante <danielabrante@protonmail.com>
2024-06-01 15:00:35 -0300

    fix: add new message error when auth exceed limit

--- lib/pages/sign_up.dart
+++ lib/pages/sign_up.dart
@@ -24,8 +24,6 @@ class SignUpPage extends StatelessWidget {
         }
       } catch (error) {
         if (error is AuthException) {
-          print(error.message);
-
           showDialog(
               context: context,
               builder: (context) => ErrorDialog(error: error));
--- lib/widgets/errors.dart
+++ lib/widgets/errors.dart
@@ -26,17 +26,17 @@ class ErrorDialog extends StatelessWidget {
 }
 
 String errorMessages(AuthException error) {
-  print(error.message);
-
   switch (error.message) {
     case "Invalid login credentials":
       return "Credenciais de login inválidas";
     case "You must provide either an email or phone number":
       return "Você deve inserir um email válido";
     case "Anonymous sign-ins are disabled":
-      return "Email ou Senha inválidos";
+      return "Email ou Senha inválido";
     case "Password should be at least 6 characters":
       return "A senha deve ter no mínimo 6 caracteres";
+    case "Email rate limit exceeded":
+      return "Não é possível criar uma conta no momento, tente novamente mais tarde";
     default:
       return "Infelizmente ocorreu um erro, tente novamente";
   }
